6b7453c188bc40f9d8b4bc16c15d7017195f1944
ManojKasunBandara <76498082+ManojKasunBandara@users.noreply.github.com>
2021-01-14 15:56:36 +0530

    Add files via upload

---
+++ Module4_solution/SpeakGoodBye.js
@@ -0,0 +1,12 @@
+
+
+// Solution
+
+(function(window) {
+	var byeSpeaker = new Object();
+	var speakWord = "Good Bye";
+	byeSpeaker.speak = function speak(name) {
+		console.log(speakWord + " " + name);
+};
+window.byeSpeaker = byeSpeaker;
+})(window);
